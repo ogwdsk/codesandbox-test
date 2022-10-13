443102d8dba48dd4163ae1e923adabbf0f0b5786
ogwdsk <ogawa@casanel.me>
2022-10-13 14:39:16 +0000

    Update 8 files

--- src/index.js
+++ src/index.js
@@ -4,7 +4,12 @@ document.getElementById("app").innerHTML = `
 <h1>Hello Vanilla!</h1>
 <div>
   We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
+  info about Parcel aaa
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
 </div>
 `;
+
+const func3 = (str1, str2) => {
+  return str1 + str2;
+};
+console.log(func3(1, 2));
